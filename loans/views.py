72fdc8c4fbaff271eba8a42a65e9728053dd2448
Davide <davide@davidevitelaru.com>
2017-05-10 00:12:00 +0100

    Created separate sign in page, divided styles

--- loans/views.py
+++ loans/views.py
@@ -2,9 +2,19 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
 
 def index(request):
+
+    return redirect('/dashboard')
+
+    context = {
+        'title': 'Sign In'
+    }
+    return render(request, 'loans/auth.html', context)
+
+def dashboard(request):
     context = {
         'title': 'Homepage'
     }
