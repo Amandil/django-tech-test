1cd805a0d11211ea137b96f556e4c3c89f601b83
Davide <davide@davidevitelaru.com>
2017-05-13 12:18:24 +0100

    Split views into UI and API

--- loans/urls.py
+++ loans/urls.py
@@ -2,16 +2,17 @@ from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 
-from . import views
+from loans import views
 
 urlpatterns = [
     # User authentication
-    url(r'^$', views.index, name='index'),
-    url(r'^register$', views.registration, name='registration'),
+    url(r'^$', views.views_ui.index, name='index'),
+    url(r'^register$', views.views_ui.registration, name='registration'),
     # Main dashboard
-    url(r'^dashboard$', views.dashboard, name='dashboard'),
+    url(r'^dashboard$', views.views_ui.dashboard, name='dashboard'),
     # Loan application
-    url(r'^apply/loan-application/([1-4]{1})/', views.loan_application, name='loan_application'),
-    url(r'^apply/loan-application/([1-4]{1})/([a-z,A-Z,0-9]{8})$', views.loan_application, name='loan_application_crn'),
-
+    url(r'^apply/loan-application/([1-4]{1})/', views.views_ui.loan_application, name='loan_application'),
+    url(r'^apply/loan-application/([1-4]{1})/([a-z,A-Z,0-9]{8})$', views.views_ui.loan_application, name='loan_application_crn'),
+    # "API"
+    url(r'^api/v1/register$', views.views_api.register)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ loans/views/__init__.py
@@ -0,0 +1,2 @@
+from . import views_ui
+from . import views_api
